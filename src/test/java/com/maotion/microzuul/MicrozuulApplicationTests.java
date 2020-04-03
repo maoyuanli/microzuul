3fe938fb2ae34e53c3dada62041f4981644f8fda
mao <maoyuan@gmail.com>
2020-04-02 21:08:02 -0400

    init

---
+++ src/test/java/com/maotion/microzuul/MicrozuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maotion.microzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrozuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
